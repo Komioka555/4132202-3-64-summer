60028ee28e79edb2560539693b3b338db19019a7
Komioka555 <630112418057@bru.ac.th>
2022-04-22 11:49:35 +0700

    lab4

---
+++ lab4/www/php/profile.php
@@ -0,0 +1,9 @@
+<?php
+
+echo"Mr.Sahaphop Ngamthongchai <br>";
+echo"ID : 630112418057 <br>";
+echo"Nickname : KIW <br>";
+echo"Address : นครราชสีมา <br>";
+echo"Favourite : เกม,อนิเมะ,ดูหนัง,ฟังเพลง <br>";
+echo"Sport : ปิงปอง,แบตมินตัน";
+echo"dream career : Animetor,youtuber,creater game";
